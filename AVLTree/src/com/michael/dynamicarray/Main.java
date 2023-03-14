b59d5629a77dd2e159ab2b100af527865af9f3d2
mhz8240 <michaelzheng119@gmail.com>
2023-03-13 19:10:04 -0700

    AVL Tree

---
+++ AVLTree/src/com/michael/dynamicarray/Main.java
@@ -0,0 +1,18 @@
+package com.michael.dynamicarray;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        AVLTree avlTree = new AVLTree();
+        avlTree.insert(7);
+        avlTree.insert(6);
+        avlTree.insert(3);
+        avlTree.print();
+        avlTree.insert(2);
+        avlTree.insert(1);
+        System.out.println();
+        avlTree.print();
+
+    }
+}
