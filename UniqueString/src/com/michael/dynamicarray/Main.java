bae14e318285e3ff64ce16a0a52c39e5f109e5b0
mhz8240 <michaelzheng119@gmail.com>
2023-03-13 19:36:28 -0700

    Determine if a string has all unique chars.

---
+++ UniqueString/src/com/michael/dynamicarray/Main.java
@@ -0,0 +1,62 @@
+package com.michael.dynamicarray;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+        String str1 = "abcd";
+        String str2 = "abca";
+        System.out.println(isUnique(str1));
+        System.out.println(isUnique(str2));
+    }
+
+//    public static boolean isUnique(String str) {
+//        Set<Character> set = new HashSet<>();
+//        for (int i = 0; i < str.length(); i++) {
+//            int size = set.size();
+//            set.add(str.charAt(i));
+//            if (set.size() == size) {
+//                return false;
+//            }
+//        }
+//
+//        return true;
+//    }
+
+//    public static boolean isUnique(String str) {
+//        int[] iArray = new int[256];
+//        for (int i = 0; i < str.length(); i++) {
+//            int n = str.charAt(i);
+//            iArray[n]++;
+//            if (iArray[n] > 1) {
+//                return false;
+//            }
+//
+//
+//
+//        }
+//        return true;
+//    }
+
+    public static boolean isUnique(String str) {
+        HashMap<Character, Integer> hashMap = new HashMap<>();
+        for (int i = 0; i < str.length(); i++) {
+            char c = str.charAt(i);
+            hashMap.put(c, hashMap.get(c) == null ? 1 : hashMap.get(c) + 1);
+
+        }
+        boolean returnValue = true;
+        for (char c : hashMap.keySet()) {
+            System.out.println(hashMap.get(c));
+            if (hashMap.get(c) > 1) {
+                returnValue = false;
+            }
+        }
+        return returnValue;
+
+    }
+}
