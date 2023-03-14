a84a5d570bb32a80ae85833649cf6519d541ce04
mhz8240 <michaelzheng119@gmail.com>
2023-03-13 19:10:24 -0700

    Binary Heap

---
+++ BinaryHeap/src/com/michael/dynamicarray/Main.java
@@ -0,0 +1,22 @@
+package com.michael.dynamicarray;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        BinaryHeap binaryHeap = new BinaryHeap();
+        binaryHeap.insert(35);
+        binaryHeap.insert(29);
+        binaryHeap.insert(42);
+        System.out.println("Extracted: " + binaryHeap.extractMax());
+//        binaryHeap.print();
+        binaryHeap.printSortedQueueInPlace();
+        binaryHeap.insert(50);
+        binaryHeap.insert(60);
+//        System.out.println();
+//        binaryHeap.printSortedQueueInPlace();
+        System.out.println();
+        binaryHeap.printSortedQueueASC();
+        binaryHeap.printSortedQueueDSC();
+    }
+}
